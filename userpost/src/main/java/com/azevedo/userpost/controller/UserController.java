96c42f727ae2f446e6484d2284745950c17de64b
theusazevedd <matheus_ma02@hotmail.com>
2026-04-19 04:37:42 -0300

    refactor: update controller to use service layer

--- userpost/src/main/java/com/azevedo/userpost/controller/UserController.java
+++ userpost/src/main/java/com/azevedo/userpost/controller/UserController.java
@@ -1,25 +1,25 @@
 package com.azevedo.userpost.controller;
 
 import com.azevedo.userpost.domain.User;
+import com.azevedo.userpost.sevice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
