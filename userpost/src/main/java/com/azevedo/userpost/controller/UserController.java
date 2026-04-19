92897be0da9b11a82983027fc6cb53851cdc9a98
theusazevedd <matheus_ma02@hotmail.com>
2026-04-19 06:08:49 -0300

    add findAll endpoint with DTO mapping

--- userpost/src/main/java/com/azevedo/userpost/controller/UserController.java
+++ userpost/src/main/java/com/azevedo/userpost/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.azevedo.userpost.controller;
 
 import com.azevedo.userpost.domain.User;
+import com.azevedo.userpost.dto.UserResponse;
+import com.azevedo.userpost.mapper.UserMapper;
 import com.azevedo.userpost.sevice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +20,14 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserResponse>> findAll() {
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+
+        List<UserResponse> response = list.stream()
+                .map(UserMapper::toDTO)
+                .toList();
+
+        return ResponseEntity.ok().body(response);
     }
 
 
