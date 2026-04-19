fc2376b13d93cf199ce597083aaf67e8ec251f40
theusazevedd <matheus_ma02@hotmail.com>
2026-04-19 03:28:38 -0300

    chore: inicializa projeto Spring Boot

---
+++ userpost/src/main/java/com/azevedo/userpost/UserpostApplication.java
@@ -0,0 +1,13 @@
+package com.azevedo.userpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserpostApplication.class, args);
+	}
+
+}
