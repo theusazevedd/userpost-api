8af791a14a40a922f42a83fe019f95359d718609
theusazevedd <matheus_ma02@hotmail.com>
2026-04-19 04:16:10 -0300

    feat: add UserController with findAll endpoint

--- userpost/src/main/java/com/azevedo/userpost/domain/User.java
+++ userpost/src/main/java/com/azevedo/userpost/domain/User.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
 
@@ -12,6 +13,7 @@ import java.util.Objects;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "users")
 public class User {
 
     @Id
